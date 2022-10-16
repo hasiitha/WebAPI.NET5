8317ea8792a2f5080ed437ab90597e82512b045b
Hasitha Attanayake <bandarahasitha68@gmail.com>
2022-10-16 10:36:42 +0530

    working CRUD functionalities .net

--- ExampleApplication/Program.cs
+++ ExampleApplication/Program.cs
@@ -1,6 +1,23 @@
+using ExampleApplication.Models;
+using ExampleApplication.Service;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<StudentDatabaseSettings>(
+    builder.Configuration.GetSection(nameof(StudentDatabaseSettings)));
+
+builder.Services.AddSingleton<IStudentDatabaseSettings>(sp => sp.GetRequiredService<IOptions<StudentDatabaseSettings>>().Value);
+
+builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("StudentDatabaseSettings:ConnectionString")));
+
+
+builder.Services.AddScoped<IStudentService, StudentService>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ ExampleApplication/Service/StudentService.cs
@@ -0,0 +1,40 @@
+﻿using ExampleApplication.Models;
+using MongoDB.Driver;
+
+namespace ExampleApplication.Service;
+
+public class StudentService : IStudentService
+{
+    private readonly IMongoCollection<Student> _students;
+
+    public StudentService(IStudentDatabaseSettings settings,IMongoClient mongoClient) {
+        var database = mongoClient.GetDatabase(settings.DatabaseName);
+        _students = database.GetCollection<Student>(settings.StudentCollectionName);
+    
+    }
+    public Student Create(Student student)
+    {
+        _students.InsertOne(student);
+        return student;
+    }
+
+    public List<Student> Get()
+    {
+       return _students.Find(student => true).ToList();
+    }
+
+    public Student Get(string id)
+    {
+        return _students.Find(student => student.Id == id).FirstOrDefault();
+    }
+
+    public void Remove(string id)
+    {
+       _students.DeleteOne(student=>student.Id == id);
+    }
+
+    public void Update(string id,Student student)
+    {
+        _students.ReplaceOne(student => student.Id == id, student);
+    }
+}
